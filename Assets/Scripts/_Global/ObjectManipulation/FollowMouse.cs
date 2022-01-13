c1e5fe42aa1a6da9f00547683dc66ccd4de4c3a7
Huszan <mateuszjacenty1@gmail.com>
2022-01-13 19:27:38 +0100

    Add ui elements for game scene
    * They are currently non functional
    * Adds build menu and info panel

---
+++ Assets/Scripts/_Global/ObjectManipulation/FollowMouse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    void Update()
+    {
+        var mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        mousePos = mousePos + new Vector3(0, 0, 10);
+        gameObject.transform.position = mousePos;
+    }
+}
