f7168376fd487933b0cf04fda68207cbee0c8c69
Huszan <mateuszjacenty1@gmail.com>
2022-02-02 11:47:44 +0100

    Add access to main canvas to main menu button

--- Assets/MainUI.cs
+++ Assets/MainUI.cs
@@ -1,16 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MainUI : MonoBehaviour
 {
     public static MainUI Instance { get; private set; }
+    public GameObject Curtain;
+    public GameObject UserBoard;
     void Awake()
     {
         if (Instance == null)
             Instance = this;
         else
             Destroy(gameObject);
+        
     }
 
 }
